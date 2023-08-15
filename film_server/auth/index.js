ce33a968a915b803553a74fa11136b94ff75915f
Maxim <maleshinmaksim@gmail.com>
2023-08-15 21:52:22 +0300

    fix:cors policy

--- film_server/auth/index.js
+++ film_server/auth/index.js
@@ -8,11 +8,16 @@ const errorMiddleware= require('./middlewares/error-middleware');
 DB_URL ='mongodb+srv://root:k46KovkUKwl3U8bk@cluster0.hf3bmfc.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT = 5000;
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/api',router);
 app.use(errorMiddleware);
 
