5f5758283bfaceb67abef31ec37d86e72caa8e7b
Maxim <maleshinmaksim@gmail.com>
2023-08-25 17:42:24 +0300

    fix:cors

--- film_server/auth/index.js
+++ film_server/auth/index.js
@@ -11,7 +11,7 @@ const PORT = 5000;
 const corsOptions ={
     origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+
 }
 const app = express()
 
