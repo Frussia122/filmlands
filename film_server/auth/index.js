6b669c89b3747eba59bc0dad6b3f1ac2ab58584f
Maxim <maleshinmaksim@gmail.com>
2023-08-25 17:58:52 +0300

    fix:cors

--- film_server/auth/index.js
+++ film_server/auth/index.js
@@ -8,16 +8,33 @@ const errorMiddleware= require('./middlewares/error-middleware');
 DB_URL ='mongodb+srv://root:k46KovkUKwl3U8bk@cluster0.hf3bmfc.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT = 5000;
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
 
+
+var allowedOrigins = ['http://localhost:3000',
+'http://yourapp.com'];
+app.use(cors({
+origin: function(origin, callback){
+// allow requests with no origin
+// (like mobile apps or curl requests)
+if(!origin) return callback(null, true);
+if(allowedOrigins.indexOf(origin) === -1){
+var msg = 'The CORS policy for this site does not ' +
+'allow access from the specified Origin.';
+return callback(new Error(msg), false);
 }
+return callback(null, true);
+}
+})); 
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+
+// }
 const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use('/api',router);
 app.use(errorMiddleware);
 
