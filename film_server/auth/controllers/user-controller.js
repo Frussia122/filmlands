2c657011f9c921fe6effee7361b6991ffe1eb11e
Maxim <maleshinmaksim@gmail.com>
2023-08-26 21:54:01 +0300

    fix: cors

--- film_server/auth/controllers/user-controller.js
+++ film_server/auth/controllers/user-controller.js
@@ -14,7 +14,7 @@ class UserController {
             }
             const {email, password} = req.body;
             const userData = await userService.registration(email,password);
-            res.cookie('refreshToken',userData.refreshToken,{maxAge:30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken',userData.refreshToken,{maxAge:30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite : none })
             return res.json(userData)
         }catch (e){
             next(e);
@@ -25,7 +25,7 @@ class UserController {
         try{
             const {email, password} = req.body;
             const userData = await userService.login(email, password);
-            res.cookie('refreshToken',userData.refreshToken,{maxAge:30 * 24 * 60 * 60 * 1000, httpOnly: true })
+            res.cookie('refreshToken',userData.refreshToken,{maxAge:30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite : none })
             return res.json(userData)
         }catch (e){
             next(e);
@@ -59,7 +59,7 @@ class UserController {
         try{
             const {refreshToken} = req.cookies;
             const userData = await userService.refresh(refreshToken);
-            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite : none });
             return res.json(userData)
         }catch (e){
             next(e);
