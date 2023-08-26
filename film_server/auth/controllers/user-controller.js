04677128c9caf2053633c09af8b5a9f550aae16c
Amidgard <44028334+Frussia122@users.noreply.github.com>
2023-08-26 19:20:46 +0300

    fix: refresh cookie

--- film_server/auth/controllers/user-controller.js
+++ film_server/auth/controllers/user-controller.js
@@ -59,7 +59,7 @@ class UserController {
         try{
             const {refreshToken} = req.cookies;
             const userData = await userService.refresh(refreshToken);
-            res.cookie('refreshToken',userData.refreshToken,{maxAge:30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true})
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'Lax' });
             return res.json(userData)
         }catch (e){
             next(e);
